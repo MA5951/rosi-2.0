8f1b2fcb68544b4c6b2ce36c68c8431b54b6b5cc
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-10 01:54:33 +0300

    vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Navbar } from "../components/navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +25,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <ToastContainer />
+        <Analytics />
       </body>
     </html>
   );
