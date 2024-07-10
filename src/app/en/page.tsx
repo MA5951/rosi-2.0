ee337c13f0854c3757a8f9763e40426edd5210fc
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-10 16:49:17 +0300

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
       </div>
 
-      {/* <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]"> */}
       <div>
         <Image
           // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -19,28 +18,7 @@ export default function Home() {
           height={500}
           priority
         />
-      </div>
-
-      {/* <div className="rounded-md py-20 px-96 bg-slate-700">
-        Pill shape
-      </div> */}
-
-      {/* <div className="rounded-md py-20 px-96 bg-slate-700">
-        <p style={{"text-align: center; padding-right: 0;"}}>
-            ROSI - Robotics Open Source Israel
-            <br />
-            הוא פרויקט אופן סורס שמטרתו יצירת תשתית משותפת בין כל קבוצות FIRST בארץ.
-            <br />
-            לאתר שלנו אפשר להעלות מצגות, קבצי CAD, קטעי קוד, הרצאות מצולמות ועוד.
-            <br />
-            כך שכל קבוצה, תוכל ללמוד מהקבוצות האחרות תוך שיתוף הידע שלה
-            <br />
-            כאשר המצב שהאתר שואף לקדם הוא העלאת הרמה של כל הקבוצות בארץ
-            <br />
-            בכל הנושאים, מהקמת פרויקטים קהילתיים ומדיה, דרך תוכנה וקוד ועד להנדסת מכונות והנדסה.
-            <br />
-          </p>
-      </div> */}
+      </div>     
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
       <a
