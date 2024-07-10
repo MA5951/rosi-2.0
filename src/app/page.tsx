2fcbafdc5f62a9e20099178bad417cfdf437f1e3
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-10 16:18:59 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (      
@@ -46,6 +48,7 @@ export default function Home() {
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
       >
         <h2 className="mb-3 text-2xl font-semibold">
+          <FontAwesomeIcon icon={faCode} className="mr-2" />
           programming{" "}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
             -&gt;
