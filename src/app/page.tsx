df60c6f95e956670b2970b3c530afa862e1843c1
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-10 16:07:47 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (      
@@ -41,6 +43,7 @@ export default function Home() {
       </div> */}
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <FontAwesomeIcon icon={faSun} className="w-6 h-6" />
       <a
         href="/programming"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
