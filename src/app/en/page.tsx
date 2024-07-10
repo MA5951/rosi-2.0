026c4ef8d95c19fe0ed085ed1d7b14dbe2648f7a
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-10 16:33:48 +0300

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode, faBolt, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faBolt, faUsers, faIndustry } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (      
@@ -84,6 +84,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
+            <FontAwesomeIcon icon={faIndustry} className="mr-2" />
             Manufacturing{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
