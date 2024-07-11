51bc9af86720c92d99ed254447c256a4288f9b7e
Asaf Meizner <asafmeizner@gmail.com>
2024-07-11 03:36:32 +0300

    more margin for upload page

--- src/app/en/contribute/page.tsx
+++ src/app/en/contribute/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-between bg-blue-50 dark:bg-slate-900 text-gray-900 dark:text-white">
       <iframe 
         src="https://docs.google.com/forms/d/e/1FAIpQLScxLoXRRI_9yuyBvwb9VYiopA-h7_5qVA6ClBU4HjPOuKL-oA/viewform" 
-        style={{height: "80vh", width: "40vw", marginTop: "10vh", borderRadius: "20px", boxShadow: isDarkMode ? "0 3px 50px rgb(255 255 255 / 0.4)" : "0 3px 50px rgb(0 0 0 / 0.4)"}}
+        style={{height: "80vh", width: "40vw", marginTop: "15vh", borderRadius: "20px", boxShadow: isDarkMode ? "0 3px 50px rgb(255 255 255 / 0.4)" : "0 3px 50px rgb(0 0 0 / 0.4)"}}
       />
     </div>
   );
