0e73310751bd39412be4c6e6ee7cdf0b8e725b7f
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-12 00:25:39 +0300

    mongo connection

---
+++ src/app/he/explore/page copy.tsx
@@ -0,0 +1,132 @@
+"use client"
+
+import { useEffect, useState } from 'react';
+import Popup from '../popup/popup';
+import { motion, AnimatePresence } from 'framer-motion';
+import { getAllArticles } from '@/db/server';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+interface Contact {
+  name: string;
+  phone: string;
+}
+
+interface Article {
+  id: string;
+  title: string;
+  photo: string;
+  team_num: string;
+  link: string;
+  contact: Contact;
+}
+
+const Home: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [selectedArticle, setSelectedArticle] = useState<Article | null>(null);
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [subject, setSubject] = useState<string>('');
+  const [search, setSearch] = useState<string>('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const fetchedArticles = await getAllArticles("hebrew", subject, "");
+      const formattedArticles = fetchedArticles.map((article) => ({
+        id: article.id,
+        title: article.title,
+        photo: article.photo,
+        team_num: "", 
+        link: article.link,
+        contact: {
+          name: article.author,
+          phone: article.phone
+        }
+      }));
+      setArticles(formattedArticles);
+      setIsLoading(false);
+    };
+
+    fetchArticles();
+  }, [subject, search]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const openPopup = (article: Article) => {
+    setSelectedArticle(article);
+  };
+
+  const closePopup = () => {
+    setSelectedArticle(null);
+  };
+
+  return (
+    <main className="flex flex-col items-center min-h-screen bg-blue-50 dark:bg-slate-900 text-gray-900 dark:text-white">
+      {isLoading && (
+        <div className="z-10 w-full max-w-5xl flex justify-center items-center mt-20">
+          <FontAwesomeIcon icon={faSpinner} className="animate-spin text-6xl text-gray-900 dark:text-white" style={{marginTop: "20vh", height: "10vh", width: "10vh" }} />
+        </div>
+      )}
+      {isLoading === false && (
+        <div>
+          <div className="bottom-text-container z-10 w-full max-w-5xl flex justify-center items-center text-3xl" style={{marginTop: "17vh", marginBottom: "5vh"}}>
+            <p className="bottom-text text-center">
+              {articles.length === 0 ? "" : "Explore all resources"} 
+            </p>
+          </div>
+          <div className="articles-container z-10 w-full max-w-5xl mt-8 px-4 flex flex-wrap justify-center items-center gap-6">
+            {articles.map((article, index) => (
+              <motion.div 
+                className={isMobile ? "article-card-mobile p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md" : "article-card p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md"} 
+                key={index}
+                onClick={() => openPopup(article)}
+                style={{ cursor: 'pointer' }}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <div className="image-container">
+                  <img
+                    src={article.photo}
+                    alt={article.team_num}
+                    className="w-full h-full object-contain rounded-t-lg"
+                  />
+                </div>
+                <div className="p-2 text-center">
+                  <p className="font-semibold article-title">{article.title}</p>
+                </div>
+              </motion.div>
+            ))}
+          </div>
+          <div className="bottom-text-container z-10 w-full max-w-5xl flex justify-center items-center text-xl" style={{marginTop: "10vh", marginBottom: "10vh"}}>
+            <p className="bottom-text text-center">
+              {articles.length === 0 ? "No articles found" : "No more articles found"}
+            </p>
+          </div>
+          <AnimatePresence>
+            {selectedArticle && (
+              <Popup 
+                title={selectedArticle.title} 
+                link={selectedArticle.link} 
+                contact={selectedArticle.contact} 
+                onClose={closePopup} 
+              />
+            )}
+          </AnimatePresence>
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/db/server.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import { prisma } from '@/db/index';
+
+export async function getAllArticles(lang: String ,subject: string, search: string) {
+    try {
+        let query: any = {};
+
+        if (subject) {
+            query = {
+                subject: subject,
+                language: lang
+            };
+        } else if (search) {
+            query = {
+                OR: [
+                    { language: lang }, 
+                    { title: { contains: search, mode: 'insensitive' } },
+                    { author: { contains: search, mode: 'insensitive' } },
+                    { description: { contains: search, mode: 'insensitive' } },
+                ],
+            };
+        } else {
+            query = {
+                language: lang
+            }
+        }
+        // Fetch articles based on the constructed query
+        const articles = await prisma.articles.findMany({
+            where: query,
+            select: {
+                id: true,
+                title: true,
+                author: true,
+                phone: true,
+                description: true,
+                subject: true,
+                photo: true,
+                link: true,
+                language: true
+            }
+        });
+
+        // Return the list of articles
+        return articles;
+    } catch (error) {
+        console.error("Error fetching articles: ", error);
+        throw new Error("Error fetching articles");
+    } finally {
+        await prisma.$disconnect();
+    }
+}
