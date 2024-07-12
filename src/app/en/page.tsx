6be9bf3f3660322dd658e1feaaa9cec45bc59207
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-12 10:34:04 +0300

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         />
       </div>     
 
-      <div className={`mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left ${isMobile ? 'grid-cols-1' : 'grid-cols-4'}`}>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="/en/programming"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -113,7 +113,7 @@ export default function Home() {
             </p>
           </a>
       </div>
-      <div className={`mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left ${isMobile ? 'grid-cols-1' : 'grid-cols-4'}`}>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="/en/community"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
