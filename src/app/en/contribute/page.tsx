866069e73739e4dd488cbb94149041e0a3c61e95
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-12 10:13:29 +0300

    Update page.tsx

--- src/app/en/contribute/page.tsx
+++ src/app/en/contribute/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme');
@@ -11,11 +12,25 @@ export default function Home() {
     setIsDarkMode(isDark);
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between bg-blue-50 dark:bg-slate-900 text-gray-900 dark:text-white">
       <iframe 
         src="https://docs.google.com/forms/d/e/1FAIpQLSfRPlzpHLt8n8kSAgucbgZLZiD3Z_Zd5s1NWPU93XD-Wd6HyQ/viewform" 
-        style={{height: "80vh", width: "40vw", marginTop: "15vh", borderRadius: "20px", boxShadow: isDarkMode ? "0 3px 50px rgb(255 255 255 / 0.4)" : "0 3px 50px rgb(0 0 0 / 0.4)"}}
+        style={{height: isMobile ? "70vh" : "80vh", width: isMobile ? "80vw" : "40vw", marginTop: "15vh", borderRadius: "20px", boxShadow: isDarkMode ? "0 3px 50px rgb(255 255 255 / 0.4)" : "0 3px 50px rgb(0 0 0 / 0.4)"}}
       />
     </div>
   );
