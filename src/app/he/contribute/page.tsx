23e7d1d4eaba661b359483e0ce0d71032581793e
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-12 10:21:01 +0300

    Update page.tsx

--- src/app/he/contribute/page.tsx
+++ src/app/he/contribute/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between bg-blue-50 dark:bg-slate-900 text-gray-900 dark:text-white">
       <iframe 
-        src="https://docs.google.com/forms/d/e/1FAIpQLScxLoXRRI_9yuyBvwb9VYiopA-h7_5qVA6ClBU4HjPOuKL-oA/viewform" 
-        style={{height: isMobile ? "70vh" : 80vh", width: isMobile ? "80vw" : "40vw", marginTop: "15vh", borderRadius: "20px", boxShadow: isDarkMode ? "0 3px 50px rgb(255 255 255 / 0.4)" : "0 3px 50px rgb(0 0 0 / 0.4)"}}
+                src="https://docs.google.com/forms/d/e/1FAIpQLScxLoXRRI_9yuyBvwb9VYiopA-h7_5qVA6ClBU4HjPOuKL-oA/viewform"
+        style={{height: isMobile ? "70vh" : "80vh", width: isMobile ? "80vw" : "40vw", marginTop: "15vh", borderRadius: "20px", boxShadow: isDarkMode ? "0 3px 50px rgb(255 255 255 / 0.4)" : "0 3px 50px rgb(0 0 0 / 0.4)"}}
       />
     </div>
   );
