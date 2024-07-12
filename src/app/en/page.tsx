aa3a7af4f4f74ec0ff9d6deb72e04c3f694dd784
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-12 10:39:44 +0300

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -43,8 +43,9 @@ export default function Home() {
           height={500}
           priority
         />
-      </div>     
-
+      </div>  
+      {!isMobile && (   
+      <div>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="/en/programming"
@@ -182,6 +183,8 @@ export default function Home() {
           </p>
         </a>
       </div>
+      </div>
+      )}
     </main>
   );
 }
