1dd7de6ef1f85b4b462249b2b8eea64f86d73ceb
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-07-14 02:11:40 +0300

    fixed text direction in popup

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -72,37 +72,39 @@ const Popup: React.FC<PopupProps> = ({ title, link, contact, description, langua
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 50 }}
         >
-          <h2 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">{title}</h2>
-          <h4 className="font-bold mb-4 text-gray-900 dark:text-white">{description}</h4>
-          <div className="flex mb-4">
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 transition-colors">
-                פתח מקור
+          <div dir={"rtl"}>
+            <h2 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">{title}</h2>
+            <h4 className="font-bold mb-4 text-gray-900 dark:text-white">{description}</h4>
+            <div className="flex mb-4">
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 transition-colors">
+                  פתח מקור
+                </button>
+              </a>
+              <button
+                onClick={toggleIframe}
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors mx-4"
+              > 
+                תצוגה מקדימה
               </button>
-            </a>
+            </div>
+            {showIframe && (
+              <div className="iframe-container mt-4">
+                <iframe src={link} className="w-full h-64 border rounded"></iframe>
+              </div>
+            )}
+            <div className="mt-4">
+              <p className="font-semibold text-gray-700 dark:text-gray-300">פרטי קשר:</p>
+              <p className="text-gray-700 dark:text-gray-300">שם: {contact.name}</p>
+              <p className="text-gray-700 dark:text-gray-300">יצירת קשר: {contact.phone}</p>
+            </div>
             <button
-              onClick={toggleIframe}
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors mx-4"
-            > 
-              תצוגה מקדימה
+              onClick={onClose}
+              className="mt-6 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors"
+            >
+              Close
             </button>
           </div>
-          {showIframe && (
-            <div className="iframe-container mt-4">
-              <iframe src={link} className="w-full h-64 border rounded"></iframe>
-            </div>
-          )}
-          <div className="mt-4">
-            <p className="font-semibold text-gray-700 dark:text-gray-300">פרטי קשר:</p>
-            <p className="text-gray-700 dark:text-gray-300">שם: {contact.name}</p>
-            <p className="text-gray-700 dark:text-gray-300">יצירת קשר: {contact.phone}</p>
-          </div>
-          <button
-            onClick={onClose}
-            className="mt-6 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors"
-          >
-            Close
-          </button>
         </motion.div>
       )}
     </div>
